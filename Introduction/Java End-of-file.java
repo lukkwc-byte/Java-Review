9ddd3a8dd3598028c75550da4f0b23a27bc019de
Kevin Luk <luk.kwc@gmail.com>
2016-05-18 02:30:34 -0400

    started and finished intro

---
+++ Introduction/Java End-of-file.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int n = 0;
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext() == true){
+            n++;
+            System.out.printf("%d %s%n", n, sc.nextLine());
+        }
+    }
+}
